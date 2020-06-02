589af78897c10dbaf2e874529e20599a10f14334
Emery Ferrari <rubiquikyt@gmail.com>
2020-06-02 18:45:59 +0900

    jse3dserver v1.0

---
+++ src/com/emeryferrari/jse3dserver/JSE3DServer.java
@@ -0,0 +1,298 @@
+package com.emeryferrari.jse3dserver;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import com.emeryferrari.jse3d.*;
+import com.emeryferrari.jse3d.network.*;
+public class JSE3DServer {
+	private boolean stop = false;
+	private int count = 0;
+	private static final JSE3DServer CLASS_OBJ = new JSE3DServer();
+	private int sceneID;
+	private Scene scene;
+	private ArrayList<String> users = new ArrayList<String>();
+	private JSE3DServer() {}
+	public static void main(String[] args) {
+		for (int i = 0; i < 1000; i++) {
+			CLASS_OBJ.users.add(null);
+		}
+		Thread.currentThread().setName("listener");
+		int port = 5107;
+		int sceneID = 0;
+		if (args.length == 1) {
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (NumberFormatException ex) {
+				System.out.println("Incorrectly formatted port. Defaulting to 5107...");
+				port = 5107;
+			}
+		} else if (args.length == 2) {
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (NumberFormatException ex) {
+				System.out.println("Incorrectly formatted port. Defaulting to 5107...");
+				port = 5107;
+			}
+			try {
+				sceneID = Integer.parseInt(args[1]);
+			} catch (NumberFormatException ex) {
+				System.out.println("Incorrectly formatted scene ID. Defaulting to 0...");
+				sceneID = 0;
+			}
+		} else {
+			System.out.println("Defaulting port to 5107...");
+			System.out.println("Defaulting scene ID to 0...");
+		}
+		System.out.println("Starting jse3d server on [127.0.0.1:" + port + "].");
+		CLASS_OBJ.start(port, sceneID);
+	}
+	public void start(int port, int sceneID) {
+		File sceneFile = new File("scenes/scn" + sceneID + ".jse3");
+		if (sceneFile.exists()) {
+			try {
+				scene = JSE3DSerializer.loadScene(sceneFile);
+			} catch (IOException ex) {
+				handleException(ex);
+				System.out.println("Error loading " + sceneFile.getAbsolutePath() + ". Defaulting scene to empty scene.");
+			} catch (ClassNotFoundException ex) {
+				handleException(ex);
+				System.exit(2);
+			}
+		} else {
+			System.out.println(sceneFile.getAbsolutePath() + " was not found. Creating file...");
+			try {
+				sceneFile.createNewFile();
+				Object3D[] objects = {};
+				Scene tmp = new Scene(objects, 5.0);
+				JSE3DSerializer.saveScene(tmp, sceneFile);
+			} catch (IOException ex) {
+				handleException(ex);
+				System.exit(3);
+			}
+		}
+		Thread consoleReader = new Thread(new ConsoleReader());
+		consoleReader.setName("console-input");
+		consoleReader.start();
+		Thread autosave = new Thread(new Autosave());
+		autosave.setName("autosave");
+		autosave.start();
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(port);
+			System.out.println("Server started.");
+		} catch (IOException ex) {
+			handleException(ex);
+			System.out.println("Server was not started successfully.");
+			System.exit(1);
+		}
+		while (!stop) {
+			try {
+				if (server != null) {
+					Socket socket = server.accept();
+					System.out.println("Found client at " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort() + ". Client ID: " + count + ".");
+					Thread client = new Thread(new ClientHandler(socket, count));
+					client.setName("client-handler-" + count);
+					count++;
+					client.start();
+				}
+			} catch (IOException ex) {
+				handleException(ex);
+			}
+		}
+	}
+	public static void handleException(Exception ex) {
+		if (!(ex.getMessage().equalsIgnoreCase("socket closed") || ex.getMessage().equalsIgnoreCase("connection reset"))) {
+			System.out.println("  ** EXCEPTION THROWN **");
+			System.out.println(String.valueOf(ex.getClass()).split(" ")[1] + " occurred in thread \"" + Thread.currentThread().getName() + "\":");
+			System.out.println("Cause: " + ex.getMessage());
+			System.out.println();
+		}
+	}
+	public class ConsoleReader implements Runnable {
+		public void run() {
+			try {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+				String line;
+				while (!stop) {
+					line = reader.readLine();
+					if (line != null) {
+						handleCommand(line);
+					}
+				}
+				System.exit(0);
+			} catch (IOException ex) {
+				handleException(ex);
+			}
+		}
+	}
+	public void handleCommand(String command) {
+		if (command.equalsIgnoreCase("stop")) {
+			stop = true;
+			System.out.println("Stopping server...");
+		} else {
+			System.out.println("Unknown command.");
+		}
+	}
+	public class ClientHandler implements Runnable {
+		private Socket socket;
+		private int clientID;
+		@SuppressWarnings("unused")
+		private String username;
+		public ClientHandler(Socket socket, int clientID) {
+			this.socket = socket;
+			this.clientID = clientID;
+		}
+		public void run() {
+			String username = null;
+			try {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				username = reader.readLine();
+				String version = reader.readLine();
+				if (!version.equals(JSE3DServerConst.VERSION)) {
+					socket.close();
+					System.out.println("[Server thread: " + Thread.currentThread().getName() + "]: Rejected incoming user " + username + " with client ID " + clientID + " as the client version is not equal to the server version.");
+				}
+				if (users.contains(username)) {
+					oos.writeObject("username-taken");
+					oos.flush();
+					System.out.println("[Server thread: " + Thread.currentThread().getName() + "]: Rejected incoming user " + username + " with client ID " + clientID + " as the username is already taken.");
+					socket.close();
+				} else if (username.equals("")) {
+					oos.writeObject("username-invalid");
+					oos.flush();
+					System.out.println("[Server thread: " + Thread.currentThread().getName() + "]: Rejected incoming user with client ID " + clientID + " as the username is not valid.");
+					socket.close();
+				} else {
+					users.set(clientID, username);
+				}
+				System.out.print("[Server thread: " + Thread.currentThread().getName() + "]: Uploading scene to user " + username + "... ");
+				oos.writeObject(scene);
+				oos.flush();
+				System.out.println("Done!");
+				Thread receiver = new Thread(new Receiver(socket, clientID, username));
+				receiver.setName("client-receiver-" + clientID);
+				receiver.start();
+				Thread monitor = new Thread(new SceneMonitor(socket));
+				monitor.setName("client-monitor-" + clientID);
+				monitor.start();
+				while (!socket.isClosed()) {
+					try {
+						Thread.sleep(17);
+					} catch (InterruptedException ex) {
+						handleException(ex);
+					}
+				}
+				oos.flush();
+				socket.close();
+				if (!(username == null)) {
+					users.set(clientID, null);
+				}
+				System.out.println("[Server thread: "+Thread.currentThread().getName() + "]: Client with client ID " + clientID + " has disconnected.");
+			} catch (IOException ex) {
+				System.out.println("[Server thread: "+Thread.currentThread().getName() + "]: Client with client ID " + clientID + " has disconnected.");
+				if (!(username == null)) {
+					users.set(clientID, null);
+				}
+			}
+		}
+	}
+	public class SceneMonitor implements Runnable {
+		private Socket socket;
+		public SceneMonitor(Socket socket) {
+			this.socket = socket;
+		}
+		public void run() {
+			boolean shouldRun1 = true;
+			if (socket.isClosed()) {
+				shouldRun1 = false;
+			}
+			if (shouldRun1) {
+				ObjectOutputStream oos = null;
+				try {
+					oos = new ObjectOutputStream(socket.getOutputStream());
+				} catch (IOException ex) {
+					handleException(ex);
+				}
+				if (oos != null) {
+					boolean shouldRun2 = true;
+					while (!stop && shouldRun2) {
+						if (socket.isClosed()) {
+							shouldRun2 = false;
+						} else {
+							try {
+								Scene temp = scene;
+								while (temp.equals(scene)) {
+									try {
+										Thread.sleep(17);
+									} catch (InterruptedException ex) {
+										handleException(ex);
+									}
+								}
+								oos.writeObject(scene);
+								oos.flush();
+							} catch (IOException ex) {
+								handleException(ex);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	public class Receiver implements Runnable {
+		private Socket socket;
+		private int clientID;
+		public Receiver(Socket socket, int clientID, String username) {
+			this.socket = socket;
+			this.clientID = clientID;
+		}
+		public void run() {
+			boolean shouldRun = true;
+			while (!stop && shouldRun) {
+				if (socket.isClosed()) {
+					shouldRun = false;
+				} else {
+					try {
+						ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+						Object object;
+						while ((object = ois.readObject()) != null) {
+							if (object instanceof Scene) {
+								System.out.println("[Server thread: " + Thread.currentThread().getName() + "]: Client with client ID " + clientID + " changed the current scene.");
+								scene = (Scene) object;
+							} else if (object instanceof Disconnect) {
+								socket.close();
+							}
+						}
+					} catch (IOException ex) {
+						handleException(ex);
+						try {
+							socket.close();
+						} catch (IOException ex2) {
+							handleException(ex2);
+						}
+					} catch (ClassNotFoundException ex) {
+						handleException(ex);
+						System.exit(2);
+					}
+				}
+			}
+		}
+	}
+	public class Autosave implements Runnable {
+		public void run() {
+			while (!stop) {
+				try {
+					JSE3DSerializer.saveScene(new File("scenes/scn" +sceneID + ".jse3"), scene);
+				} catch (IOException ex) {
+					handleException(ex);
+				}
+				try {
+					Thread.sleep(JSE3DServerConst.AUTOSAVE_INTERVAL);
+				} catch (InterruptedException ex) {
+					handleException(ex);
+				}
+			}
+		}
+	}
+}
---
+++ src/com/emeryferrari/jse3dserver/JSE3DServerConst.java
@@ -0,0 +1,5 @@
+package com.emeryferrari.jse3dserver;
+public class JSE3DServerConst {
+	public static final int AUTOSAVE_INTERVAL = 15000;
+	public static final String VERSION = "v1.0";
+}
