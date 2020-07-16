c417cf6120ea403007c31df9c4a2951754af00bb
Emery Ferrari <rubiquikyt@gmail.com>
2020-07-16 21:14:03 +0900

    jse3dserver v1.1.262 beta

--- src/com/emeryferrari/jse3dserver/JSE3DServerConst.java
+++ src/com/emeryferrari/jse3dserver/JSE3DServerConst.java
@@ -1,5 +1,5 @@
 package com.emeryferrari.jse3dserver;
 public class JSE3DServerConst {
 	public static final int AUTOSAVE_INTERVAL_MILLIS = 15000;
-	public static final String API_VERSION = "v1.1.261";
+	public static final String API_VERSION = "v1.1.262";
 }
